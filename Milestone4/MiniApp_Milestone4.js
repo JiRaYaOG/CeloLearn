0545897d3606c4e678a6fd355d04dbded674cbc7
JiRaYa_OG <115741454+JiRaYaOG@users.noreply.github.com>
2025-11-02 15:50:12 +0100

    Refactor showWallet and simulateTransaction functions

--- Milestone4/MiniApp_Milestone4.js
+++ Milestone4/MiniApp_Milestone4.js
@@ -1,7 +1,6 @@
-﻿function showWallet() {
+function showWallet() {
   document.getElementById("wallet").innerText = "Test Wallet: 0x1234...ABCD";
 }
-
 
 function simulateTransaction() {
   document.getElementById("transaction").innerText = "Transaction simulated: 10 cUSD sent!";
